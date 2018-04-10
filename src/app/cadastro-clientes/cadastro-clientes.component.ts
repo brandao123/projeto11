894dd55e489ca53023c3ce970fd22b3f7d1a86b7
gabriel <gabrielcandidobrandao@gmail.com>
2018-04-09 21:35:07 -0300

    trabalho

---
+++ src/app/cadastro-clientes/cadastro-clientes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClienteService } from '../services/cliente.service';
+
+@Component({
+  selector: 'app-cadastro-clientes',
+  templateUrl: './cadastro-clientes.component.html',
+  styleUrls: ['./cadastro-clientes.component.css']
+})
+export class CadastroClientesComponent implements OnInit {
+
+formGroup : FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private clienteService : ClienteService) {
+    this.formGroup = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      telefone: ['', [Validators.required,]],
+      email: ['', [Validators.required, Validators.email]]
+    }) 
+  }
+
+  ngOnInit() {
+  }
+
+  cadastrar(){
+    this.clienteService.insert(this.formGroup.value)
+    .subscribe(response => {
+      console.log("Cadastrado com sucesso");
+    }, error => {
+      console.log("Erro ao cadastrar");
+    })
+    console.log(this.formGroup.value);
+  }
+
+}
